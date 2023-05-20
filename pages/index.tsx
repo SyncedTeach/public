a2567a027ca6348d85c31f00ecd77f13e97f477e
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-05-20 21:16:27 +0700

    change: update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,29 @@
 import Head from "next/head";
+import Image from "next/image";
 import * as React from "react";
 import Button from "@mui/material/Button";
 import landing from "@/styles/landing.module.css";
 import LandingUserOptions from "@/components/LandingUserOptions";
+import "@/styles/globals.css";
 
 export default function Home() {
   const userOptions = [
     {
       name: "Teacher",
       link: "/user/login?type=teacher",
-      url: "/static/imgs/teacher.png",
+      url: "/static/images/teacher.png",
       width: "20%",
     },
     {
       name: "Student",
       link: "/user/login/?type=student",
-      url: "/static/imgs/student.png",
+      url: "/static/images/student.png",
       width: "20%",
     },
     {
       name: "Parent",
       link: "/user/login/?type=parent",
-      url: "/static/imgs/parent.png",
+      url: "/static/images/parent.png",
       width: "20%",
     },
   ];
@@ -41,6 +43,15 @@ export default function Home() {
           <p>Get started</p>
           <div className={landing.btncontainer}></div>
         <LandingUserOptions userOptions={userOptions} />
+        <br></br>
+        <div style={{"padding":"4px"}}className="text--size-medium">
+          Made by <a href="https://mistertfy64.com"><Image src="/static/images/mistertfy64.png" alt="mistertfy64" width="121" height="20" style={{"top":"5px","position":"relative"}}></Image></a> and <a className="text--link" href="https://github.com/EpicCatto">EpicCatto</a>
+        </div>
+        <div className="text--size-small">
+          <a className="text--link" href="https://github.com/SyncedTeach/public">Frontend Repository</a> • <a className="text--link" href="https://github.com/SyncedTeach/api">API Repository</a> • <a className="text--link" href="https://github.com/SyncedTeach/core">Installation Instructions</a>
+          <br></br>
+          Made as a project for a software competition, but free for anyone to use commercially.
+        </div>
         </div>
       </div>
     </>
