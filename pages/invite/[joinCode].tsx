4ec8bf54407318be3bbe4a91bb8e9d28a1a9a9b2
Pat <72650902+EpicCatto@users.noreply.github.com>
2023-06-16 18:15:33 +0700

    Update [joinCode].tsx

--- pages/invite/[joinCode].tsx
+++ pages/invite/[joinCode].tsx
@@ -4,7 +4,7 @@ import Head from "next/head";
 import Button from "@mui/material/Button";
 import styles from "@/styles/landing.module.css";
 import settings from "@/utils/settings";
-import { LinearProgress, Backdrop, Paper } from "@mui/material";
+import { LinearProgress, Backdrop, Paper, Alert } from "@mui/material";
 import Image from "next/image";
 
 interface IData {
@@ -20,30 +20,66 @@ export default function Join() {
   });
 
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   const joinCode = router.query.joinCode as string;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(settings.config.api_route + "/v1/user/self", {
+  const fetchData = async () => {
+    if(!router.isReady || !joinCode) return;
+    console.log("Fetching data");
+    if (!joinCode)return;
+    try {
+      const response = await fetch(
+        settings.config.api_route + "/v1/user/self",
+        {
           method: "GET",
           credentials: "include",
+        }
+      );
+      const data = await response.json();
+
+      const groupsData = await fetch(
+        settings.config.api_route + "/v1/groups/code/" + joinCode,
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      const groups = await groupsData.json();
+      console.log(groupsData);
+      if (!groups.success || !groups.group || !groups) {
+        return setData({
+          title: "Error",
+          description:
+            "Invalid join code. Please tell your teacher to generate a new one.",
         });
-        const data = await response.json();
-
-        if (data.success !== true) {
-          return router.push("/user/login");
-        }
-
-        if (!data.data.membership) {
-          return router.push("/dashboard");
-        }
-      } catch (error) {
-        console.error("Error:", error);
-        // Handle any errors here
       }
-    };
+
+
+      setData({
+        title: "You have been invited to join " + groups.group.name + "!",
+        description: "Click the button below to join the group.",
+      });
+
+      if (data.success !== true) {
+        return setError("You are not logged in. Please login before joining the group.");
+      }
+
+      if (!data.data.membership) {
+        return setError("You are not a student, teacher, or parent. Please contact your teacher to join the group.");
+      }
+
+      setLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle any errors here
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+
     console.log("Join code: " + joinCode);
   }, [router.isReady]);
 
@@ -74,7 +110,7 @@ export default function Join() {
       <div className={styles.section1}>
         <div className={styles.strcontainer}>
           <Paper elevation={3} style={{ padding: "1rem" }}>
-          <Image
+            <Image
               src="/favicon.ico"
               alt="SyncedTeach logo"
               width={64}
@@ -82,13 +118,17 @@ export default function Join() {
               priority
               style={{ margin: "10px", borderRadius: "10px" }}
             />
-            <p style={{ fontSize: "30px", fontWeight: "bold" }}>{data?.title}</p>
+            <p style={{ fontSize: "30px", fontWeight: "bold" }}>
+              {data?.title}
+            </p>
             <p style={{ fontSize: "20px" }}> {data?.description} </p>
+            
+            {error && <Alert severity="error">{error}</Alert>}
 
             <Button
               variant="contained"
               color="warning"
-              style={{ marginTop: "10px"}}
+              style={{ marginTop: "10px" }}
               onClick={() => router.push("/dashboard")}
             >
               Back
@@ -102,9 +142,8 @@ export default function Join() {
             >
               Join
             </Button>
-
-            </Paper>
-          </div>
+          </Paper>
+        </div>
       </div>
     </>
   );
